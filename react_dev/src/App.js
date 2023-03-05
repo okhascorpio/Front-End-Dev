287c68c9e6b1bea37081bd6550b798dcd72bf5dd
okhascorpio <okhascorpio@gmail.com>
2023-03-05 22:25:23 +0200

    wellcome to react world

---
+++ react_dev/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from './components/Hello'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <Hello />
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ react_dev/src/components/Hello.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Hello = () => {
+    // return (
+    //     <div>
+    //         <h1>Hello World</h1>
+    //     </div>
+    // )
+    return React.createElement('div',{id:"div "},React.createElement("h2",{class:"hw"},"Hello World"))
+}
+
+export default Hello
