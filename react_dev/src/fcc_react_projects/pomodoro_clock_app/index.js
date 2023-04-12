0ff62e3d1c8747f458f6723f146040c5e54e5519
okhascorpio <okhascorpio@gmail.com>
2023-04-12 20:53:02 +0300

    pomodoro clock update

--- react_dev/src/fcc_react_projects/pomodoro_clock_app/index.js
+++ react_dev/src/fcc_react_projects/pomodoro_clock_app/index.js
@@ -1,25 +1,39 @@
+import React from 'react';
+
 //import beepFile from 'https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav';
-const audio = document.getElementById("timer-beep");
+const audioUrl =
+  "https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav";
 
 function ClockApp() {
-  const [sessionTime, setSessionTime] = React.useState(5);
-  const [breakTime, setBreakTime] = React.useState(3);
+  const [sessionTime, setSessionTime] = React.useState(25);
+  const [breakTime, setBreakTime] = React.useState(5);
   const [clockActive, setClockActive] = React.useState(false);
   const [sessionActive, setSessionActive] = React.useState(true);
   const [displayTime, setDisplayTime] = React.useState(sessionTime * 60);
+  const audioBeep = React.useRef();
 
   const timeSettings = [
-    { title: "Break", time: breakTime, setTime: setBreakTime },
-    { title: "Session", time: sessionTime, setTime: setSessionTime },
+    { id: "break", title: "Break", time: breakTime, setTime: setBreakTime },
+    {
+      id: "session",
+      title: "Session",
+      time: sessionTime,
+      setTime: setSessionTime,
+    },
   ];
-  function handlePlayPause(val) {
-    setClockActive(val === "play");
+
+  function handlePlayPause() {
+    setClockActive(!clockActive);
   }
 
   function handleReset() {
+    setClockActive(false);
+    setSessionActive(true);
     setSessionTime(25);
     setBreakTime(5);
     setDisplayTime(25 * 60);
+    audioBeep.current.pause();
+    audioBeep.current.currentTime = 0;
   }
 
   React.useEffect(() => {
@@ -29,13 +43,14 @@ function ClockApp() {
   }, [sessionActive]);
 
   return (
-    <div className="container text-center mt-5">
-      <h1>25 + 5 Clock</h1>
+    <div className="container text-center p-5" >
+      <h1 className="title">Pomodoro Clock</h1>
 
-      <div className="set-intervals d-flex justify-content-between w-50 mx-auto mt-5">
+      <div className="set-intervals d-flex justify-content-between mx-auto mt-5">
         {timeSettings.map((setting) => (
           <SetTime
             key={setting.title}
+            id={setting.id}
             title={setting.title}
             time={setting.time}
             setTime={setting.setTime}
@@ -46,7 +61,7 @@ function ClockApp() {
           />
         ))}
       </div>
-      <div className="timer-window border w-25 d-flex flex-column mx-auto mt-5">
+      <div className="timer-window d-flex flex-column mx-auto mt-5">
         <div className="timer-display">
           <DisplayTime
             displayTime={displayTime}
@@ -54,14 +69,20 @@ function ClockApp() {
             sessionActive={sessionActive}
             setSessionActive={setSessionActive}
             clockActive={clockActive}
+            audioBeep={audioBeep}
           />
         </div>
-        <div className="play-pause-button">
-          <button onClick={() => handlePlayPause("play")}>Play</button>
-          <button onClick={() => handlePlayPause("pause")}>Pause</button>
-          <button onClick={() => handleReset()}>Reset</button>
+        <div className="play-pause-reset d-flex justify-content-center mt-4">
+          <button id="start_stop" className= {`btn btn-success rounded-pill ${clockActive && "btn-warning"}`} onClick={() => handlePlayPause()}>
+            {clockActive ? <i className="fa fa-pause"></i> : <i className="fa fa-play"></i>}
+          </button><span></span>
+          <button id="reset" className="btn btn-danger rounded-pill" onClick={() => handleReset()}>
+          <i className="fa fa-clock-rotate-left"></i>
+          </button>
+          <audio id="beep" src={audioUrl} type="audio" ref={audioBeep} />
         </div>
       </div>
+      <p className="author text-center">Designed and Coded by<br/>Faheem Ahmed</p>
     </div>
   );
 }
@@ -75,6 +96,7 @@ function DisplayTime({
   sessionActive,
   setSessionActive,
   clockActive,
+  audioBeep,
 }) {
   React.useEffect(() => {
     let timerId;
@@ -82,9 +104,13 @@ function DisplayTime({
     if (clockActive && displayTime >= 0) {
       timerId = setInterval(() => {
         setDisplayTime((prevTime) => prevTime - 1);
-        displayTime === 0 && setSessionActive(!sessionActive) && audio.play();
+        if (displayTime === 0) {
+          audioBeep.current.play();
+          setSessionActive(!sessionActive);
+        }
       }, 1000);
     }
+    !clockActive && clearInterval(timerId);
     return () => {
       clearInterval(timerId);
     };
@@ -92,8 +118,12 @@ function DisplayTime({
 
   return (
     <div className="display-time">
-      <h1 className="clock-title">{sessionActive ? "Session" : "Break"}</h1>
-      <h1 className="clock-time">{formatTime(displayTime)}</h1>
+      <h1 id="timer-label" className="clock-title">
+        {sessionActive ? "Session" : "Break"}
+      </h1>
+      <h1 id="time-left" className="clock-time">
+        {formatTime(displayTime)}
+      </h1>
     </div>
   );
 }
@@ -104,7 +134,8 @@ function DisplayTime({
 function formatTime(val) {
   let str = "";
   let seconds = val % 60;
-  let minutes = parseInt(val / 60) % 60;
+  let tempMinutes = parseInt(val / 60) % 60;
+  let minutes = val !== 0 && val % 3600 === 0 ? 60 : tempMinutes;
   const addLeadingZeroes = (time) => {
     return time < 10 ? "0" + time : time;
   };
@@ -116,22 +147,32 @@ function formatTime(val) {
 /////////////////////////////////////////////////////////////////////////////////////////////
 
 function SetTime({
+  id,
   title,
   time,
   setTime,
   clockActive,
   sessionActive,
   displayTime,
-  setDisplayTime,
+  setDisplayTime
 }) {
   const ChangeTime = (val) => {
     !clockActive &&
-      time > 1 &&
-      (setTime((prevTime) => prevTime + val),
+      time >= 1 &&
+      time <= 60 &&
+      (setTime((prevTime) => {
+        return (prevTime === 60 && val === 1) || (prevTime < 2 && val === -1)
+          ? prevTime
+          : prevTime + val;
+      }),
       setDisplayTime(
-        sessionActive && title === "Session"
+        sessionActive &&
+          title === "Session" &&
+          ((time < 60 && val === 1) || (time > 1 && val === -1))
           ? (time + val) * 60
-          : !sessionActive && title === "Break"
+          : !sessionActive &&
+            title === "Break" &&
+            ((time < 60 && val === 1) || (time > 1 && val === -1))
           ? (time + val) * 60
           : displayTime
       ));
@@ -139,21 +180,38 @@ function SetTime({
 
   return (
     <div className="timer-controls">
-      <h1 className="timer-title">{title}</h1>
-      <button className="timer-decrement" onClick={() => ChangeTime(-1)}>
-        <span className="timer-symbol">-</span>
+      <h1 id={id + "-label"} className="timer-title">
+        {title}
+      </h1>
+      <div className="button-block d-flex align-items-center">
+      <button
+        id={id + "-decrement"}
+        className="timer-decrement btn btn-info rounded"
+        onClick={() => ChangeTime(-1)}
+      >
+        <span className="timer-symbol"><i className="fa fa-arrow-down"></i></span>
       </button>
-      <span className="controller-time">{time}</span>
-      <button className="timer-increment" onClick={() => ChangeTime(+1)}>
-        <span className="timer-symbol">+</span>
+      <span id={id + "-length"} className="controller-time mx-3">
+        {time}
+      </span>
+      <button
+        id={id + "-increment"}
+        className="timer-increment btn btn-info"
+        onClick={() => ChangeTime(+1)}
+      >
+        <span className="timer-symbol"><i className="fa fa-arrow-up"></i></span>
       </button>
+      </div>
+      
     </div>
   );
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////////////////////////////////
-
+/*
 const root = document.getElementById("root");
 const rootElement = ReactDOM.createRoot(root);
 rootElement.render(<ClockApp />);
+*/
+export default ClockApp;
