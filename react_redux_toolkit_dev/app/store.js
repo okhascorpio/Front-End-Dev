82f5b6b9345582c4b38b44e3d508cf6707553aa5
okhascorpio <okhascorpio@gmail.com>
2023-04-18 18:28:17 +0300

    using react toolkit

--- react_redux_toolkit_dev/app/store.js
+++ react_redux_toolkit_dev/app/store.js
@@ -1,14 +1,18 @@
 const configureStore = require('@reduxjs/toolkit').configureStore
 //const { getDefaultMiddleware } = require('@reduxjs/toolkit')
-const cakeReducer = require('../features/cake/cakeSlice')
-const icecreamReducer = require('../features/icecream/icecreamSlice')
 const reduxLogger = require('redux-logger')
 //const logger = reduxLogger.createLogger()
+
+const cakeReducer = require('../features/cake/cakeSlice')
+const icecreamReducer = require('../features/icecream/icecreamSlice')
+const userReducer = require('../features/user/userSlice')
+
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
+        user: userReducer,
     },
     //middleware: (getDefaultMiddleware)=> getDefaultMiddleware().concat(logger),
 })
--- react_redux_toolkit_dev/index.js
+++ react_redux_toolkit_dev/index.js
@@ -1,15 +1,20 @@
 const store = require('./app/store')
 const cakeActions = require('./features/cake/cakeSlice').cakeActions
 const icecreamActions = require('./features/icecream/icecreamSlice').icecreamActions
+const fetchUsers = require('./features/user/userSlice').fetchUsers
+
 console.log('initital state ',store.getState())
 const unsubscribe = store.subscribe(()=> {
     console.log('Updated state ', store.getState())
 })
 
-store.dispatch(cakeActions.ordered())
+/* store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(cakeActions.ordered())
 store.dispatch(icecreamActions.ordered())
 store.dispatch(cakeActions.restocked(6))
-store.dispatch(icecreamActions.restocked(5))
-unsubscribe()
+store.dispatch(icecreamActions.restocked(5)) */
+
+store.dispatch(fetchUsers())
+
+//unsubscribe()
